1f1417d9d17b62a6511a6f8e6f84a0b05bf47aa0
Benedikt Lang <github@benediktlang.de>
2015-10-23 10:40:34 +0200

    first release

--- main.go
+++ main.go
@@ -8,18 +8,19 @@ import (
 	"strings"
 )
 
-type GithubKeys []struct {
-	ID  int    `json:"id"`
+type githubKeys []struct {
 	Key string `json:"key"`
 }
 
+type githubError struct {
+	Message string `json:"message"`
+}
+
 func usage() {
-	fmt.Fprintf(os.Stderr, "Usage: %s [USERNAME]\n")
+	fmt.Fprintf(os.Stderr, "Usage: %s [USERNAME]\n", os.Args[0])
 }
 
 func main() {
-
-	// No username
 	if len(os.Args) < 2 {
 		usage()
 		os.Exit(2)
@@ -32,14 +33,24 @@ func main() {
 	}
 	resp, err := http.Get(fmt.Sprintf("https://api.github.com/users/%s/keys", strings.ToLower(user)))
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %s\n")
+		fmt.Fprintf(os.Stderr, "Error requesting api: %s\n", err)
 		os.Exit(1)
 	}
 	dec := json.NewDecoder(resp.Body)
-	var ghKeys GithubKeys
+	if resp.StatusCode != http.StatusOK {
+		var ghError githubError
+		err = dec.Decode(&ghError)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unknown error occurred.\n")
+			os.Exit(1)
+		}
+		fmt.Fprintf(os.Stderr, "API Error: %s.\n", ghError.Message)
+		os.Exit(1)
+	}
+	var ghKeys githubKeys
 	err = dec.Decode(&ghKeys)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %s\n")
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 		os.Exit(1)
 	}
 	for _, key := range ghKeys {
